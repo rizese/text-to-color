4fb51344304bd161394599ced620f226069e6664
Ricky Franzese <thechosenrick+gh@gmail.com>
2025-03-03 23:07:27 -0500

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,32 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <meta
+          property="og:title"
+          content="Text-to-Color: Input text, get color"
+        />
+        <meta property="og:description" content="Input text, get color" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://text-to-color.com" />
+        <meta
+          property="og:image"
+          content="https://text-to-color.com/og-image.png"
+        />
+        <meta
+          property="og:image:alt"
+          content="Text-to-Color application screenshot"
+        />
+        <meta property="og:site_name" content="Text-to-Color" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Text-to-Color: Input text, get color"
+        />
+        <meta name="twitter:description" content="Input text, get color" />
+        <meta
+          name="twitter:image"
+          content="https://text-tocolor.com/og-image.png"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${oi.variable} antialiased`}
